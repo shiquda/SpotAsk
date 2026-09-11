97a4d529ac685f13077de0567f10342a39614de2
shiquda <shiquda@gmail.com>
2026-09-12 02:09:57 +0800

    chore: update Homebrew cask for v0.2.3

--- Casks/spotask.rb
+++ Casks/spotask.rb
@@ -1,13 +1,13 @@
 cask "spotask" do
-  version "0.2.2"
+  version "0.2.3"
 
   on_arm do
-    sha256 "7694809d875e2ccd7987f3b62de4ba3dd463b604d97941e92774b580f1ad2987"
+    sha256 "f634e821c183a66cb57bf7fd19aea6fd772f6629ded69bc4f88e47f840c540d7"
 
     url "https://github.com/shiquda/SpotAsk/releases/download/v#{version}/SpotAsk-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "09dea9072b4f8500560783ae916c7076fd346f3ce27c0d286778235e1bc58dd9"
+    sha256 "006429e96f1124bf28b5b5b593fb8dd637aa2b418e3d98f6bf868b6a83397d91"
 
     url "https://github.com/shiquda/SpotAsk/releases/download/v#{version}/SpotAsk-#{version}-x86_64.dmg"
   end
